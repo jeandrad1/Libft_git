358f52a7cdf78d64872b21061c2d3035c58802cc
jeandrad1 <jesusandradeperez@gmail.com>
2023-12-05 23:20:50 +0100

    quick fix

--- ft_memset.c
+++ ft_memset.c
@@ -4,17 +4,16 @@
 
 void *ft_memset(void *str, int c, size_t n)
 {
-	size_t i;
-
-	i = 0;
-	while (i < n)
+	unsigned char * s = str;
+	while (n > 0)
 	{
-		((unsigned char *)str)[i] = c;
-		i++;
+		*s = (unsigned char)c;
+		s++;
+		n--;
 	}
 	return (str);
 }
-/*
+
 #include <stdio.h>
 #include <string.h>
 int main()
@@ -25,4 +24,3 @@ int main()
     printf("After ft_memset():  %s\n", str); 
 	return (0);
 }
-*/
