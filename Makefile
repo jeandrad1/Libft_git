10aaebc2d08154c8d76792c30ef35128310849cf
jeandrad1 <152317568+jeandrad1@users.noreply.github.com>
2023-12-08 23:37:23 +0100

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 NAME = libft.a
 #Compilador y flags
 
@@ -31,3 +32,38 @@ fclean: clean
 re: fclean all
 
 .PHONY: all clean fclean re
+=======
+NAME = libft.a
+#Compilador y flags
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+#Archivos
+
+SRC =	ft_isalnum.c ft_isalpha.c ft_isdigit.c ft_isascii.c ft_isprint.c ft_strlen.c ft_strlcat.c ft_strlcpy.c \
+		ft_toupper.c ft_tolower.c ft_strchr.c ft_memset.c ft_bzero.c  ft_strrchr.c ft_strncmp.c ft_memchr.c
+
+OBJ = $(SRC:.c=.o)
+
+# Reglas
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(AR) rcs $@ $^
+
+#Salida .o
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+#Limpia libreria y .o
+clean:
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+#Reconstruye
+re: fclean all
+
+.PHONY: all clean fclean re
+>>>>>>> fb86e458cadab77659c8654238c9689182309bc8
--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,11 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 12:54:45 by jeandrad          #+#    #+#             */
+<<<<<<< HEAD
+/*   Updated: 2023/12/08 21:43:33 by jeandrad         ###   ########.fr       */
+=======
 /*   Updated: 2023/12/06 13:32:49 by jeandrad         ###   ########.fr       */
+>>>>>>> fb86e458cadab77659c8654238c9689182309bc8
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,6 +18,45 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+<<<<<<< HEAD
+	const char	*str;
+	size_t		i;
+
+	i = 0;
+	str = (const char *) s;
+	while (i < n)
+	{
+		if (str[i] == c)
+			return ((void *)(str + i));
+		i++;
+	}
+	return (0);
+
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function declaration
+void	*ft_memchr(const void *s, int c, size_t n);
+
+int main() {
+	const char *str = "Hello, World!";
+	int search_char = 'o';
+	size_t search_len = strlen(str);
+
+	// Call ft_memchr function
+	void *result = ft_memchr(str, search_char, search_len);
+
+	if (result != NULL) {
+		printf("Character '%c' found at index %ld\n", search_char, (char *)result - str);
+	} else {
+		printf("Character '%c' not found\n", search_char);
+	}
+
+	return 0;
+=======
 	unsigned char	*str;
 
 	str = s;
@@ -28,4 +71,5 @@ void	*ft_memchr(const void *s, int c, size_t n)
 		return (str);
 	return (0);
 
+>>>>>>> fb86e458cadab77659c8654238c9689182309bc8
 }
