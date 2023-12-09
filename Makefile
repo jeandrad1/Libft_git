7f9c8f8fc288c5a51ae09d272e671153124b6944
jeandrad1 <jesusandradeperez@gmail.com>
2023-12-09 22:50:08 +0100

    some changes

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 NAME = libft.a
 #Compilador y flags
 
@@ -33,39 +32,3 @@ fclean: clean
 re: fclean all
 
 .PHONY: all clean fclean re
-=======
-NAME = libft.a
-#Compilador y flags
-
-CC = gcc
-CFLAGS = -Wall -Wextra -Werror
-#Archivos
-
-SRC =	ft_isalnum.c ft_isalpha.c ft_isdigit.c ft_isascii.c ft_isprint.c ft_strlen.c ft_strlcpy.c \
-		ft_toupper.c ft_tolower.c ft_strchr.c ft_memset.c ft_bzero.c  ft_strrchr.c ft_strncmp.c ft_memchr.c \
-		ft_memcmp.c ft_atoi.c ft_strnstr.c
-
-OBJ = $(SRC:.c=.o)
-
-# Reglas
-all: $(NAME)
-
-$(NAME): $(OBJ)
-	$(AR) rcs $@ $^
-
-#Salida .o
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
-
-#Limpia libreria y .o
-clean:
-	rm -f $(OBJ)
-
-fclean: clean
-	rm -f $(NAME)
-
-#Reconstruye
-re: fclean all
-
-.PHONY: all clean fclean re
->>>>>>> 5f895a932239130d538251fd5c3f3f839078c67c
--- ft_memchr.c
+++ ft_memchr.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -7,12 +6,12 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 12:54:45 by jeandrad          #+#    #+#             */
-/*   Updated: 2023/12/09 11:23:30 by jeandrad         ###   ########.fr       */
+/*   Updated: 2023/12/09 22:48:53 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-#include <stddef.h>
 #include "libft.h"
+#include <stddef.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
@@ -28,73 +27,29 @@ void	*ft_memchr(const void *s, int c, size_t n)
 		i++;
 	}
 	return (0);
+
 }
 
-/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// Function declaration
+void	*ft_memchr(const void *s, int c, size_t n);
 
 int main() {
 	const char *str = "Hello, World!";
 	int search_char = 'o';
 	size_t search_len = strlen(str);
+
+	// Call ft_memchr function
 	void *result = ft_memchr(str, search_char, search_len);
+
 	if (result != NULL) {
 		printf("Character '%c' found at index %ld\n", search_char, (char *)result - str);
 	} else {
 		printf("Character '%c' not found\n", search_char);
 	}
+
 	return 0;
 }
-=======
-/******************************************************************************/
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/06 12:54:45 by jeandrad          #+#    #+#             */
-/*   Updated: 2023/12/09 11:23:30 by jeandrad         ###   ########.fr       */
-/*                                                                            */
-/******************************************************************************/
-
-#include <stddef.h>
-#include "libft.h"
-
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	const char	*str;
-	size_t		i;
-
-	i = 0;
-	str = (const char *) s;
-	while (i < n)
-	{
-		if (str[i] == c)
-			return ((void *)(str + i));
-		i++;
-	}
-	return (0);
-}
-
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main() {
-	const char *str = "Hello, World!";
-	int search_char = 'o';
-	size_t search_len = strlen(str);
-	void *result = ft_memchr(str, search_char, search_len);
-	if (result != NULL) {
-		printf("Character '%c' found at index %ld\n", search_char, (char *)result - str);
-	} else {
-		printf("Character '%c' not found\n", search_char);
-	}
-	return 0;
-}
->>>>>>> 5f895a932239130d538251fd5c3f3f839078c67c
-*/
