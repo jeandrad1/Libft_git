2954515eae87e0f05f1a89e0677ecbb457bd542b
jeandrad1 <jesusandradeperez@gmail.com>
2023-12-22 21:47:51 +0100

    night update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
@@ -6,29 +6,33 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 09:04:20 by jeandrad          #+#    #+#             */
-/*   Updated: 2023/12/22 13:55:14 by jeandrad         ###   ########.fr       */
+/*   Updated: 2023/12/22 20:23:17 by jeandrad         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*father;
-	t_list	*new;
+	t_list	*new_list;
+	t_list	*new_node;
+	void	*set;
 
-	father = ft_lstnew(NULL);
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = NULL;
 	while (lst)
 	{
-		new = ft_lstnew((*f)(lst->content));
-		if (!(new))
+		set = f(lst->content);
+		new_node = ft_lstnew(set);
+		if (!new_node)
 		{
-			ft_lstclear(&father, del);
-			free(father);
-			return (NULL);
+			del(set);
+			ft_lstclear(&new_list, (*del));
+			return (new_list);
 		}
-		ft_lstadd_back(&father, new);
+		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
 	}
-	return (father);
+	return (new_list);
 }
